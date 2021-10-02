e3a293c3db706fe7f0ecdb23a91d2a5e5457c8e3
Michael Staatz <michael@staatzstreich.de>
2021-10-02 21:57:52 +0200

    Finished Example 003

--- examples/003/build-querystring.js
+++ examples/003/build-querystring.js
@@ -1,1 +1,16 @@
 const querystring = require('querystring');
+
+const apiHost = 'https://jira.my-company.com/rest/api/latest/search?jql=';
+
+const jqlParams = {
+    assignee: 'shaun.stone',
+    startAt: 2,
+    maxResults: 2
+};
+
+const apiUrl = `${apiHost}"${querystring.stringify(jqlParams)}"`;
+
+// querystring is deprecated, one could use this
+const apiUrl2 = `${apiHost}"${(new URLSearchParams(jqlParams)).toString()}"`;
+
+console.log(`My JQL API call is ${apiUrl2}`);
--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,20 @@
 const querystring = require('querystring');
+
+const url = 'http://www.opencanvas.co.uk?myName=Shaun&myAge=28&comment=Yes+I+am+getting+old';
+
+const parsedUrl = querystring.parse(url.substring(url.indexOf('?') + 1));
+
+console.log(`Hi my name is ${parsedUrl.myName}`);
+
+console.log(`I am ${parsedUrl.myAge}`);
+
+console.log(`Oh and... ${parsedUrl.comment}`);
+
+// One can use this because querystring is deprecated
+const param = new URLSearchParams(url.substring(url.indexOf('?') + 1));
+
+console.log(`Hi my name is ${param.get('myName')}`);
+
+console.log(`I am ${param.get('myAge')}`);
+
+console.log(`Oh and... ${param.get('comment')}`);
