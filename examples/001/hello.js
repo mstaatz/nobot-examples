1b9a58743011dfddabb162936a79074b42f9272f
Michael Staatz <michael@staatzstreich.de>
2021-10-02 18:46:10 +0200

    Finished Example 001

---
+++ examples/001/hello.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+
+const [name] = args;
+
+if (name === undefined) {
+    console.error('Please pass a name e.g. node hello.js Michael');
+    process.exit(0);
+}
+
+console.log(`Good day to you, ${name}`);
