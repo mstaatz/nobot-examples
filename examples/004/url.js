1fd3b9311e1c9405f0106f9c088fc1222c1a15cc
Michael Staatz <michael@staatzstreich.de>
2021-10-03 07:17:13 +0200

    Finished Example 0004

--- examples/004/url.js
+++ examples/004/url.js
@@ -1,1 +1,40 @@
-const url = require('url');
+//const url = require('url');
+
+const args = process.argv.slice(2);
+
+const [urlEntered] = args;
+
+if (urlEntered === undefined) {
+    console.error('Please pass an url e.g. https://www.google.de/search?q=stranger+things');
+    process.exit(0);
+}
+
+
+
+/*
+const {
+    protocol, slashes, host, query, href
+} = url.parse(urlEntered);
+
+console.log(`Using protocol: ${protocol}`);
+
+console.log(`Using slashes: ${slashes}`);
+
+console.log(`Host: ${host}`);
+
+console.log(`Query: ${query}`);
+
+console.log(`HREF: ${href}`);
+*/
+
+const url = new URL(urlEntered);
+
+console.log(`Using protocol: ${url.protocol}`);
+
+//console.log(`Using slashes: ${url.slashes}`);
+
+console.log(`Host: ${url.host}`);
+
+console.log(`Query: ${url.search}`);
+
+console.log(`HREF: ${url.href}`);
